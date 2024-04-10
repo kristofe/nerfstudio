df44f74e63e43e056fdf5077d905c93484e9b80e
Kristofer <kristofe72@gmail.com>
2024-04-10 13:57:25 -0400

    Start of an SH exporter

--- nerfstudio/exporter/exporter_utils.py
+++ nerfstudio/exporter/exporter_utils.py
@@ -27,7 +27,8 @@ import numpy as np
 import pymeshlab
 import torch
 from jaxtyping import Float
-from rich.progress import BarColumn, Progress, TaskProgressColumn, TextColumn, TimeRemainingColumn
+from rich.progress import (BarColumn, Progress, TaskProgressColumn, TextColumn,
+                           TimeRemainingColumn)
 from torch import Tensor
 
 from nerfstudio.cameras.cameras import Cameras
@@ -78,6 +79,231 @@ def get_mesh_from_filename(filename: str, target_num_faces: Optional[int] = None
         ms.meshing_decimation_quadric_edge_collapse(targetfacenum=target_num_faces)
     mesh = ms.current_mesh()
     return get_mesh_from_pymeshlab_mesh(mesh)
+
+def generate_spherical_harmonics_cloud(
+    pipeline: Pipeline,
+    num_points: int = 1000000,
+    remove_outliers: bool = True,
+    estimate_normals: bool = False,
+    reorient_normals: bool = False,
+    rgb_output_name: str = "rgb",
+    depth_output_name: str = "depth",
+    normal_output_name: Optional[str] = None,
+    use_bounding_box: bool = True,
+    bounding_box_min: Optional[Tuple[float, float, float]] = None,
+    bounding_box_max: Optional[Tuple[float, float, float]] = None,
+    crop_obb: Optional[OrientedBox] = None,
+    std_ratio: float = 10.0,
+) -> o3d.geometry.PointCloud:
+    """Generate a point cloud from a nerf.
+
+    Args:
+        pipeline: Pipeline to evaluate with.
+        num_points: Number of points to generate. May result in less if outlier removal is used.
+        remove_outliers: Whether to remove outliers.
+        reorient_normals: Whether to re-orient the normals based on the view direction.
+        estimate_normals: Whether to estimate normals.
+        rgb_output_name: Name of the RGB output.
+        depth_output_name: Name of the depth output.
+        normal_output_name: Name of the normal output.
+        use_bounding_box: Whether to use a bounding box to sample points.
+        bounding_box_min: Minimum of the bounding box.
+        bounding_box_max: Maximum of the bounding box.
+        std_ratio: Threshold based on STD of the average distances across the point cloud to remove outliers.
+
+    Returns:
+        Point cloud.
+    """
+
+    progress = Progress(
+        TextColumn(":cloud: Computing Spherical Harmonics Cloud :cloud:"),
+        BarColumn(),
+        TaskProgressColumn(show_speed=True),
+        TimeRemainingColumn(elapsed_when_finished=True, compact=True),
+        console=CONSOLE,
+    )
+    points = []
+    rgbs = []
+    normals = []
+    view_directions = []
+
+    CONSOLE.print("HELLO WORLD")
+    CONSOLE.print("HELLO WORLD")
+    CONSOLE.print("HELLO WORLD")
+    CONSOLE.print("HELLO WORLD")
+    CONSOLE.print("HELLO WORLD")
+    CONSOLE.print("HELLO WORLD")
+    #TESTING FORCING POINT SAMPLING AT WITH A SPECIFIC DIRECTION
+    from nerfstudio.cameras.rays import RaySamples
+    from nerfstudio.model_components.ray_samplers import UniformSampler
+
+    #Probably can get away with thousands of samples > 10K
+    num_directions = 1024
+    results = []
+    grid_res = 16
+    linepoints = torch.linspace(-1, 1, grid_res, device=pipeline.device)
+    x,y,z = torch.meshgrid(linepoints, linepoints, linepoints, indexing='ij')
+    positions = torch.stack((x.flatten(), y.flatten(), z.flatten()), dim=1)
+    num_rays = positions.shape[0]
+    #Samples have frustums which store the directions.  So you should only sample
+    #each ray once with the desired direction
+    #the rays will be of zero direction
+    #the origins will be the sample location because the rays are zero length
+    pixel_areas = torch.ones((num_rays, 1), device=pipeline.device)
+    nears = torch.zeros((num_rays, 1), device=pipeline.device)
+    fars = torch.zeros((num_rays, 1), device=pipeline.device)
+    camera_indices = torch.zeros((num_rays, 1), dtype=torch.long, device=pipeline.device)
+
+    with progress as progress_bar:
+        task = progress_bar.add_task("Computing Spherical Harmonics Cloud", total=num_directions)
+        for i in range(num_directions):
+            #random uniform directions
+            random_vector = torch.rand((1,3), device=pipeline.device)
+            rand_direction = random_vector / torch.linalg.norm(random_vector, dim=1, keepdim=True)
+
+            view_directions = torch.ones((num_rays, 3), device=pipeline.device)
+            view_directions = view_directions * rand_direction
+
+
+            bundle = RayBundle(
+                origins = positions, directions = view_directions, pixel_area = pixel_areas, nears=nears, fars=fars, camera_indices=camera_indices
+            )
+            #sampler = UniformSampler(num_samples = 1, train_stratified=False)
+            #ray_samples = sampler(bundle)
+            with torch.no_grad():
+                outputs = pipeline.model(bundle)
+
+            rgba = pipeline.model.get_rgba_image(outputs, rgb_output_name)
+            rgbs = rgba[..., :3]
+            results.append(rgbs)
+            progress.advance(task, 1)
+    rgbs_mean = torch.stack(results, dim=0).mean(dim=0)
+        
+    #END TESTING FORCING POINT SAMPLING AT WITH A SPECIFIC DIRECTION
+    import open3d as o3d
+
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(positions.double().cpu().numpy())
+    pcd.colors = o3d.utility.Vector3dVector(rgbs_mean.double().cpu().numpy())
+
+    CONSOLE.print("[bold green]:white_check_mark: Done Computing Spherical Harmonics Cloud")
+    return pcd
+
+    if use_bounding_box and (crop_obb is not None and bounding_box_max is not None):
+        CONSOLE.print("Provided aabb and crop_obb at the same time, using only the obb", style="bold yellow")
+    with progress as progress_bar:
+        task = progress_bar.add_task("Generating Spherical Harmonics Cloud", total=num_points)
+        while not progress_bar.finished:
+            normal = None
+
+            with torch.no_grad():
+
+                ray_bundle, _ = pipeline.datamanager.next_train(0)
+                assert isinstance(ray_bundle, RayBundle)
+                #Create a new ray bundle: 
+                outputs = pipeline.model(ray_bundle)
+                #Look at (nerfacto.py:298 get_outputs(raybundle) base_field.py:114 forward(ray_samples) and  nerfacto_field.py:230 get_output(ray_samples, density_embedding))
+                #colors = pipeline.model.field.mlp_head(samples)
+            if rgb_output_name not in outputs:
+                CONSOLE.rule("Error", style="red")
+                CONSOLE.print(f"Could not find {rgb_output_name} in the model outputs", justify="center")
+                CONSOLE.print(f"Please set --rgb_output_name to one of: {outputs.keys()}", justify="center")
+                sys.exit(1)
+            if depth_output_name not in outputs:
+                CONSOLE.rule("Error", style="red")
+                CONSOLE.print(f"Could not find {depth_output_name} in the model outputs", justify="center")
+                CONSOLE.print(f"Please set --depth_output_name to one of: {outputs.keys()}", justify="center")
+                sys.exit(1)
+            rgba = pipeline.model.get_rgba_image(outputs, rgb_output_name)
+            depth = outputs[depth_output_name]
+            if normal_output_name is not None:
+                if normal_output_name not in outputs:
+                    CONSOLE.rule("Error", style="red")
+                    CONSOLE.print(f"Could not find {normal_output_name} in the model outputs", justify="center")
+                    CONSOLE.print(f"Please set --normal_output_name to one of: {outputs.keys()}", justify="center")
+                    sys.exit(1)
+                normal = outputs[normal_output_name]
+                assert (
+                    torch.min(normal) >= 0.0 and torch.max(normal) <= 1.0
+                ), "Normal values from method output must be in [0, 1]"
+                normal = (normal * 2.0) - 1.0
+            point = ray_bundle.origins + ray_bundle.directions * depth
+            view_direction = ray_bundle.directions
+
+            # Filter points with opacity lower than 0.5
+            mask = rgba[..., -1] > 0.5
+            point = point[mask]
+            view_direction = view_direction[mask]
+            rgb = rgba[mask][..., :3]
+            if normal is not None:
+                normal = normal[mask]
+
+            if use_bounding_box:
+                if crop_obb is None:
+                    comp_l = torch.tensor(bounding_box_min, device=point.device)
+                    comp_m = torch.tensor(bounding_box_max, device=point.device)
+                    assert torch.all(
+                        comp_l < comp_m
+                    ), f"Bounding box min {bounding_box_min} must be smaller than max {bounding_box_max}"
+                    mask = torch.all(torch.concat([point > comp_l, point < comp_m], dim=-1), dim=-1)
+                else:
+                    mask = crop_obb.within(point)
+                point = point[mask]
+                rgb = rgb[mask]
+                view_direction = view_direction[mask]
+                if normal is not None:
+                    normal = normal[mask]
+
+            points.append(point)
+            rgbs.append(rgb)
+            view_directions.append(view_direction)
+            if normal is not None:
+                normals.append(normal)
+            progress.advance(task, point.shape[0])
+    points = torch.cat(points, dim=0)
+    rgbs = torch.cat(rgbs, dim=0)
+    view_directions = torch.cat(view_directions, dim=0).cpu()
+
+    import open3d as o3d
+
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(points.double().cpu().numpy())
+    pcd.colors = o3d.utility.Vector3dVector(rgbs.double().cpu().numpy())
+
+    ind = None
+    if remove_outliers:
+        CONSOLE.print("Cleaning Spherical Harmonics Cloud")
+        pcd, ind = pcd.remove_statistical_outlier(nb_neighbors=20, std_ratio=std_ratio)
+        print("\033[A\033[A")
+        CONSOLE.print("[bold green]:white_check_mark: Cleaning Spherical Harmonics Cloud")
+        if ind is not None:
+            view_directions = view_directions[ind]
+
+    # either estimate_normals or normal_output_name, not both
+    if estimate_normals:
+        if normal_output_name is not None:
+            CONSOLE.rule("Error", style="red")
+            CONSOLE.print("Cannot estimate normals and use normal_output_name at the same time", justify="center")
+            sys.exit(1)
+        CONSOLE.print("Estimating Spherical Harmonics Cloud Normals")
+        pcd.estimate_normals()
+        print("\033[A\033[A")
+        CONSOLE.print("[bold green]:white_check_mark: Estimating Spherical Harmonics Cloud Normals")
+    elif normal_output_name is not None:
+        normals = torch.cat(normals, dim=0)
+        if ind is not None:
+            # mask out normals for points that were removed with remove_outliers
+            normals = normals[ind]
+        pcd.normals = o3d.utility.Vector3dVector(normals.double().cpu().numpy())
+
+    # re-orient the normals
+    if reorient_normals:
+        normals = torch.from_numpy(np.array(pcd.normals)).float()
+        mask = torch.sum(view_directions * normals, dim=-1) > 0
+        normals[mask] *= -1
+        pcd.normals = o3d.utility.Vector3dVector(normals.double().cpu().numpy())
+
+    return pcd
 
 
 def generate_point_cloud(
--- nerfstudio/scripts/exporter.py
+++ nerfstudio/scripts/exporter.py
@@ -35,11 +35,15 @@ from typing_extensions import Annotated, Literal
 
 from nerfstudio.cameras.rays import RayBundle
 from nerfstudio.data.datamanagers.base_datamanager import VanillaDataManager
-from nerfstudio.data.datamanagers.parallel_datamanager import ParallelDataManager
+from nerfstudio.data.datamanagers.parallel_datamanager import \
+    ParallelDataManager
 from nerfstudio.data.scene_box import OrientedBox
 from nerfstudio.exporter import texture_utils, tsdf_utils
-from nerfstudio.exporter.exporter_utils import collect_camera_poses, generate_point_cloud, get_mesh_from_filename
-from nerfstudio.exporter.marching_cubes import generate_mesh_with_multires_marching_cubes
+from nerfstudio.exporter.exporter_utils import (
+    collect_camera_poses, generate_point_cloud,
+    generate_spherical_harmonics_cloud, get_mesh_from_filename)
+from nerfstudio.exporter.marching_cubes import \
+    generate_mesh_with_multires_marching_cubes
 from nerfstudio.fields.sdf_field import SDFField  # noqa
 from nerfstudio.models.splatfacto import SplatfactoModel
 from nerfstudio.pipelines.base_pipeline import Pipeline, VanillaPipeline
@@ -88,9 +92,105 @@ def validate_pipeline(normal_method: str, normal_output_name: str, pipeline: Pip
 
 
 @dataclass
-class ExportPointCloud(Exporter):
+class ExportSphericalHarmonicsCloud(Exporter):
     """Export NeRF as a point cloud."""
 
+    num_points: int = 1000000
+    """Number of points to generate. May result in less if outlier removal is used."""
+    remove_outliers: bool = True
+    """Remove outliers from the SH cloud."""
+    reorient_normals: bool = True
+    """Reorient sh cloud normals based on view direction."""
+    normal_method: Literal["open3d", "model_output"] = "model_output"
+    """Method to estimate normals with."""
+    normal_output_name: str = "normals"
+    """Name of the normal output."""
+    depth_output_name: str = "depth"
+    """Name of the depth output."""
+    rgb_output_name: str = "rgb"
+    """Name of the RGB output."""
+    use_bounding_box: bool = True
+    """Only query points within the bounding box"""
+    bounding_box_min: Optional[Tuple[float, float, float]] = (-1, -1, -1)
+    """Minimum of the bounding box, used if use_bounding_box is True."""
+    bounding_box_max: Optional[Tuple[float, float, float]] = (1, 1, 1)
+    """Maximum of the bounding box, used if use_bounding_box is True."""
+
+    obb_center: Optional[Tuple[float, float, float]] = None
+    """Center of the oriented bounding box."""
+    obb_rotation: Optional[Tuple[float, float, float]] = None
+    """Rotation of the oriented bounding box. Expressed as RPY Euler angles in radians"""
+    obb_scale: Optional[Tuple[float, float, float]] = None
+    """Scale of the oriented bounding box along each axis."""
+    num_rays_per_batch: int = 32768
+    """Number of rays to evaluate per batch. Decrease if you run out of memory."""
+    std_ratio: float = 10.0
+    """Threshold based on STD of the average distances across the sh cloud to remove outliers."""
+    save_world_frame: bool = False
+    """If set, saves the sh cloud in the same frame as the original dataset. Otherwise, uses the
+    scaled and reoriented coordinate space expected by the NeRF models."""
+
+    def main(self) -> None:
+        """Export sh cloud."""
+
+        if not self.output_dir.exists():
+            self.output_dir.mkdir(parents=True)
+
+        _, pipeline, _, _ = eval_setup(self.load_config) # Do we have to load all the data????
+
+        validate_pipeline(self.normal_method, self.normal_output_name, pipeline)
+
+        # Increase the batchsize to speed up the evaluation.
+        assert isinstance(pipeline.datamanager, (VanillaDataManager, ParallelDataManager))
+        assert pipeline.datamanager.train_pixel_sampler is not None
+        pipeline.datamanager.train_pixel_sampler.num_rays_per_batch = self.num_rays_per_batch
+
+        # Whether the normals should be estimated based on the sh cloud.
+        estimate_normals = self.normal_method == "open3d"
+        crop_obb = None
+        if self.obb_center is not None and self.obb_rotation is not None and self.obb_scale is not None:
+            crop_obb = OrientedBox.from_params(self.obb_center, self.obb_rotation, self.obb_scale)
+        pcd = generate_spherical_harmonics_cloud(
+            pipeline=pipeline,
+            num_points=self.num_points,
+            remove_outliers=self.remove_outliers,
+            reorient_normals=self.reorient_normals,
+            estimate_normals=estimate_normals,
+            rgb_output_name=self.rgb_output_name,
+            depth_output_name=self.depth_output_name,
+            normal_output_name=self.normal_output_name if self.normal_method == "model_output" else None,
+            use_bounding_box=self.use_bounding_box,
+            bounding_box_min=self.bounding_box_min,
+            bounding_box_max=self.bounding_box_max,
+            crop_obb=crop_obb,
+            std_ratio=self.std_ratio,
+        )
+        if self.save_world_frame:
+            # apply the inverse dataparser transform to the sh cloud
+            points = np.asarray(pcd.points)
+            poses = np.eye(4, dtype=np.float32)[None, ...].repeat(points.shape[0], axis=0)[:, :3, :]
+            poses[:, :3, 3] = points
+            poses = pipeline.datamanager.train_dataparser_outputs.transform_poses_to_original_space(
+                torch.from_numpy(poses)
+            )
+            points = poses[:, :3, 3].numpy()
+            pcd.points = o3d.utility.Vector3dVector(points)
+
+        torch.cuda.empty_cache()
+
+        CONSOLE.print(f"[bold green]:white_check_mark: Generated {pcd}")
+        CONSOLE.print("Saving SH Cloud...")
+        tpcd = o3d.t.geometry.PointCloud.from_legacy(pcd)
+        # The legacy PLY writer converts colors to UInt8,
+        # let us do the same to save space.
+        tpcd.point.colors = (tpcd.point.colors * 255).to(o3d.core.Dtype.UInt8)  # type: ignore
+        o3d.t.io.write_point_cloud(str(self.output_dir / "sh cloud.ply"), tpcd)
+        print("\033[A\033[A")
+        CONSOLE.print("[bold green]:white_check_mark: Saving SH Cloud")
+
+@dataclass
+class ExportPointCloud(Exporter):
+    """Export NeRF as a point cloud."""
     num_points: int = 1000000
     """Number of points to generate. May result in less if outlier removal is used."""
     remove_outliers: bool = True
@@ -598,6 +698,7 @@ class ExportGaussianSplat(Exporter):
 
 Commands = tyro.conf.FlagConversionOff[
     Union[
+        Annotated[ExportSphericalHarmonicsCloud, tyro.conf.subcommand(name="spherical-harmonics-cloud")],
         Annotated[ExportPointCloud, tyro.conf.subcommand(name="pointcloud")],
         Annotated[ExportTSDFMesh, tyro.conf.subcommand(name="tsdf")],
         Annotated[ExportPoissonMesh, tyro.conf.subcommand(name="poisson")],
@@ -615,9 +716,32 @@ def entrypoint():
 
 
 if __name__ == "__main__":
+
+    # Begin debugging only code
+    import os
+    import sys
+    print(os.getcwd())  
+    os.chdir("experiments")
+    sys.argv = [sys.argv[0], "spherical-harmonics-cloud",
+                "--load-config", "outputs/unnamed/nerfacto/2024-04-05_140210/config.yml",
+                "--output-dir", "exports/pcd/",
+                "--num-points",  "1000000",
+                "--remove-outliers", "True",
+                "--normal-method", "open3d",
+                "--use_bounding_box", "False",
+                "--save-world-frame", "False",
+                "--obb_center", "0.0000000000", "0.0000000000", "0.0000000000",
+                "--obb_rotation", "0.0000000000", "0.0000000000", "0.0000000000", 
+                "--obb_scale", "1.0000000000", "1.0000000000", "1.0000000000"
+
+                ]
+    # End debugging only code
     entrypoint()
 
 
 def get_parser_fn():
     """Get the parser function for the sphinx docs."""
     return tyro.extras.get_parser(Commands)  # noqa
+
+
+#ns-export spherical-harmonics-cloud --load-config outputs/unnamed/nerfacto/2024-04-04_140226/config.yml --output-dir exports/pcd/ --num-points 1000000 --remove-outliers True --normal-method open3d --use_bounding_box False --save-world-frame False --obb_center 0.0000000000 0.0000000000 0.0000000000 --obb_rotation 0.0000000000 0.0000000000 0.0000000000 --obb_scale 1.0000000000 1.0000000000 1.0000000000
